91ff4b9c2699cbe685fd3a455ffa976db20a0142
Arafat Alam <arafatalom97@gmail.com>
2025-04-07 23:08:45 +0600

    added an add or out money

--- javascript/home.js
+++ javascript/home.js
@@ -1,1 +1,28 @@
-console.log('home added')
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault()
+  
+    
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+
+        if(pinNumberInput === '1234'){
+            console.log('you have added money')
+            const balance = document.getElementById('account-balance').innerText;
+
+            console.log(balance)
+
+            const addMoneyNumber = parseFloat(addMoneyInput)
+            const balanceNumber = parseFloat(balance)
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);   
+            
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+
+
+
+
+
+})
